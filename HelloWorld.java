9e921866b6d245b6a9e52aaf8581ab666cf6559a
rajitha2125 <109386453+rajitha2125@users.noreply.github.com>
2022-10-24 11:30:56 -0400

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World");
+        System.out.println(" Hello How are you");
+      System.out.println(" Hello my new java program")
+    }
+
+}
